4e87c3986cb3ba5ee2b0263859b0bae197c2c755
zachj <zachj@dropbox.com>
2018-07-20 17:57:33 -0700

    First push! Probably should have done this earlier
    The site has just one page and really just one main component.

---
+++ src/components/Resources/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import './style.css';
+
+class Resources extends Component {
+
+  render() {
+    return (
+      <div className="app">
+        this is the resources page
+      </div>
+    );
+  }
+}
+
+export default Resources;
---
+++ src/components/TextBlock/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import './Hero.css';
+
+class Hero extends Component {
+
+  render() {
+
+    return (
+      <div className="text-block">
+        {
+          !this.props.section ? null : (
+            <div className="text-block__section">
+              {this.props.section}
+            </div>
+          )
+        }
+        { this.props.children }
+      </div>
+    );
+  }
+}
+
+export default Hero;
